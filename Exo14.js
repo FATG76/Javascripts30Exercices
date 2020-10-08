e5c990e3355634487dd48c1f6a1562b3c1c32e46
Franck Tchitembo <franckarmel@gmail.com>
2020-10-08 14:45:38 -0400

    DOM et Fonction

--- Exo14.js
+++ Exo14.js
@@ -1,7 +1,7 @@
 const monButton = document.querySelector("button");
 const monDivResulta = document.querySelector("div");
 monButton.addEventListener("click", function(){
-     var txt =""
+    var txt =""
     for(var i=1; i<=10; i++){
                 
          txt+=`3 * ${i} = ${3*i} <br/>`;
