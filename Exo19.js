1f529276245c8d6007e65015472a063519d4465c
Franck Tchitembo <franckarmel@gmail.com>
2020-10-09 09:51:06 -0400

    Adding the recursive function

--- Exo19.js
+++ Exo19.js
@@ -1,9 +1,28 @@
 //Factoriel d'un nombre
 var readline = require("readline-sync");
 var chiffreSaisi = parseInt(readline.question("Factorielle de :  "));
-produit = 1;
-for(var i=1; i<=chiffreSaisi; i++){
-    produit *=i;
-    console.log(`Etape ${i}: ${produit}`);
+// var produit = 1;
+// for(var i=1; i<=chiffreSaisi; i++){
+//     produit *=i;
+//     console.log(`Etape ${i}: ${produit}`);
+// }
+// console.log(`Le résultat du factorielle de ${chiffreSaisi} est ${produit}`);
+
+
+//Fonction recursive
+
+var resultat = factorielle(chiffreSaisi);
+console.log(`Le résultat du factorielle de ${chiffreSaisi} est ${resultat}`);
+
+function factorielle(chiffreSaisi){
+    if(chiffreSaisi>1){
+        return chiffreSaisi*factorielle(chiffreSaisi-1);
+    }
+    else{
+        return 1;
+    }
+
+
 }
-console.log(`Le résultat du factorielle de ${chiffreSaisi} est ${produit}`);
+
+
